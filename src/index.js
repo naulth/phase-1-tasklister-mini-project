c224129c300d2778ab006c968d87998b3ff2a18e
Hayden Nault <naulth@gmail.com>
2023-02-01 11:35:27 -0500

    submitting lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const li =document.createElement("li");
+    newEntryText = document.getElementById('new-task-description').value;
+    li.textContent = newEntryText
+    const ul = document.querySelector("ul");
+    ul.appendChild(li);
+  })
 });
+
+
